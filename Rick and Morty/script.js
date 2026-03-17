05ba598fc25d4c3bce517317d55deef905d6a6a9
majianglin <majianglin@totemtec.com>
2026-03-17 16:40:56 +0800

    Update script.js

--- Rick and Morty/script.js
+++ Rick and Morty/script.js
@@ -38,11 +38,12 @@ childrenClasses.forEach(className => {
 
 const pageMain = document.getElementsByClassName('page__main');
 if (pageMain && pageMain.length > 0) {
-    pageMain[0].style.padding='0';
-    pageMain[0].style.borderRadius='0';
+    pageMain[0].style.padding = '0';
+    pageMain[0].style.borderRadius = '0';
+    pageMain[0].style.backgroundColor = 'white';
     if (pageMain[0].parentNode) {
-        pageMain[0].parentNode.style.margin='0';
-        pageMain[0].parentNode.style.border='0';
+        pageMain[0].parentNode.style.margin = '0';
+        pageMain[0].parentNode.style.border = '0';
     }
 }
 
@@ -54,10 +55,28 @@ for (const element of mainContainers) {
 
 const resizableContainer = document.getElementsByClassName('resizable-container');
 if (resizableContainer && resizableContainer.length > 0) {
-    resizableContainer[0].style.width='100%';
-    resizableContainer[0].style.margin='0';
-    resizableContainer[0].style.top='0'; 
+    resizableContainer[0].style.width = '100%';
+    resizableContainer[0].style.margin = '0';
+    resizableContainer[0].style.top = '0';
+    resizableContainer[0].style.border = '0';
+    resizableContainer[0].style.borderShadow = '0';
 }
+
+const title = "S01E01 - Pilot";
+const desc = ```Rick takes Morty on a trip to another dimension to find seeds to "mega-trees," while Jerry and Beth argue over Rick's influence over their son.```;
+
+const firstHeading = document.getElementById('firstHeading');
+if (firstHeading && firstHeading.hasChildNodes) {
+    var childNodes = firstHeading.childNodes;
+
+    childNodes.forEach(function (child) {
+        if (child.nodeType === Node.ELEMENT_NODE) {
+            child.textContent = title;
+        }
+    });
+}
+
+
 
 
 
