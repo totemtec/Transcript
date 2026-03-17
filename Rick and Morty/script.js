6054e7ef9a97c196e0d9aaa949dedb74b1a3333f
majianglin <majianglin@totemtec.com>
2026-03-17 16:05:46 +0800

    Update script.js

--- Rick and Morty/script.js
+++ Rick and Morty/script.js
@@ -1,7 +1,7 @@
 const classes = ['notifications-placeholder', 'global-top-navigation', 'global-explore-navigation', 'global-footer'
     , 'page-footer', 'page__right-rail', 'bottom-ads-container', 'page-side-tools__wrapper', 'infobox-interior'
     , 'community-header-wrapper', 'Userbox', 'fandom-community-header__background', 'skip-link'
-    , 'top_leaderboard-odyssey-wrapper'
+    , 'top_leaderboard-odyssey-wrapper', 'page-header__top', 'page-header__page-subtitle'
 ];
 classes.forEach(className => {
     const elements = document.getElementsByClassName(className);
@@ -10,11 +10,19 @@ classes.forEach(className => {
     }
 });
 
-const ids = ['p-views', 'Transcript', 'Site_navigation'];
+const ids = ['community-navigation', 'p-views'];
 ids.forEach(id => {
     const element = document.getElementById(id);
     if (element) {
         element.remove();
+    }
+});
+
+const childrenIds = ['Transcript', 'Site_navigation'];
+childrenIds.forEach(id => {
+    const element = document.getElementById(id);
+    if (element && element.parentNode) {
+        element.parentNode.remove();
     }
 });
 
@@ -26,19 +34,30 @@ childrenClasses.forEach(className => {
     }
 });
 
-// const age_gate = document.getElementById('age-gate');
-// if (age_gate) {
-//     age_gate.parentNode.remove();
-// }
 
 
-// const pageHeader = document.getElementById('page-header');
-// if (pageHeader) {
-//     pageHeader.remove();
-// }
+const pageMain = document.getElementsByClassName('page__main');
+if (pageMain && pageMain.length > 0) {
+    pageMain[0].style.padding='0';
+    pageMain[0].style.borderRadius='0';
+    if (pageMain[0].parentNode) {
+        pageMain[0].parentNode.style.margin='0';
+        pageMain[0].parentNode.style.border='0';
+    }
+}
 
+const mainContainers = document.getElementsByClassName('main-container');
+for (const element of mainContainers) {
+    element.style.marginLeft = '0';
+    element.style.width = '100%';
+}
 
-
+const resizableContainer = document.getElementsByClassName('resizable-container');
+if (resizableContainer && resizableContainer.length > 0) {
+    resizableContainer[0].style.width='100%';
+    resizableContainer[0].style.margin='0';
+    resizableContainer[0].style.top='0'; 
+}
 
 
 
